fa030615b0b413e25d2f13aea10fc5e6c573cdb2
kazu48 <kazu48jugemu@gmail.com>
2020-05-27 22:15:44 +0900

    current change

--- users/apps.py
+++ users/apps.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 
@@ -9,15 +8,3 @@ class UsersConfig(AppConfig):
     """
     name = 'users'
     verbose_name = 'ユーザー管理'
-=======
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    """
-    アプリケーション構成クラス
-    管理画面での表示名を指定する
-    """
-    name = 'users'
-    verbose_name = 'ユーザー管理'
->>>>>>> origin/master
