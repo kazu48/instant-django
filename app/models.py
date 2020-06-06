6aaab426e5ad59992f4eb07f419eb8c9a9c0c792
kazu48 <kazu48jugemu@gmail.com>
2020-06-06 10:06:50 +0900

    model

--- app/models.py
+++ app/models.py
@@ -12,85 +12,36 @@ class Item(models.Model):
     https://docs.djangoproject.com/ja/2.1/ref/models/fields/
     """
 
-    # サンプル項目1 文字列
-    sample_1 = models.CharField(
-        verbose_name='サンプル項目1 文字列',
-        max_length=20,
+    SEX_CHOICES = (
+        (1, '男性'),
+        (2, '女性'),
+        (3, 'その他'),
+    )
+
+    name = models.CharField(
+        verbose_name='名前',
+        max_length=200,
+    )
+    age = models.IntegerField(
+        verbose_name='年齢',
+        validators=[validators.MinValueValidator(1)],
         blank=True,
         null=True,
     )
-
-    # サンプル項目2 メモ
-    sample_2 = models.TextField(
-        verbose_name='サンプル項目2 メモ',
+    sex = models.IntegerField(
+        verbose_name='性別',
+        choices=SEX_CHOICES,
+        default=1
+    )
+    memo = models.TextField(
+        verbose_name='備考',
+        max_length=300,
         blank=True,
         null=True,
     )
-
-    # サンプル項目3 整数
-    sample_3 = models.IntegerField(
-        verbose_name='サンプル項目3 整数',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目4 浮動小数点
-    sample_4 = models.FloatField(
-        verbose_name='サンプル項目4 浮動小数点',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目5 固定小数点
-    sample_5 = models.DecimalField(
-        verbose_name='サンプル項目5 固定小数点',
-        max_digits=5,
-        decimal_places=2,
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目6 ブール値
-    sample_6 = models.BooleanField(
-        verbose_name='サンプル項目6 ブール値',
-    )
-
-    # サンプル項目7 日付
-    sample_7 = models.DateField(
-        verbose_name='サンプル項目7 日付',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目8 日時
-    sample_8 = models.DateTimeField(
-        verbose_name='サンプル項目8 日時',
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目9 選択肢（固定）
-    sample_9_choice = (
-        (1, '選択１'),
-        (2, '選択２'),
-        (3, '選択３'),
-    )
-
-    sample_9 = models.IntegerField(
-        verbose_name='サンプル項目9_選択肢（固定）',
-        choices=sample_9_choice,
-        blank=True,
-        null=True,
-    )
-
-    # サンプル項目9 選択肢（マスタ連動）
-    sample_10 = models.ForeignKey(
-        User,
-        verbose_name='サンプル項目10_選択肢（マスタ連動）',
-        blank=True,
-        null=True,
-        related_name='sample_10',
-        on_delete=models.SET_NULL,
+    created_at = models.DateTimeField(
+        verbose_name='登録日',
+        auto_now_add=True
     )
 
     # 以下、管理項目
